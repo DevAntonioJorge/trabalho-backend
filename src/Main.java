35c84a52563a318dda4be8a352b0e0826abfb572
Pedro <pedro.tpereira@souunit.com.br>
2025-05-05 19:58:38 -0300

    Update Main.Java
    Correção para poder executar a main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class Main{
-    public void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
 
         Scanner myScanner = new Scanner(System.in);
         GerenciadorUsuario usuarios = new GerenciadorUsuario();
